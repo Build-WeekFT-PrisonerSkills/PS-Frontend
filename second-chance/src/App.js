d91260817d90e577eef6f6b626d39e08cb4a3238
cjogles <jackson.ogles28@gmail.com>
2019-11-18 11:50:21 -0700

    Added Formik Register Form with Styled Component styling from emotion/styled

--- second-chance/src/App.js
+++ second-chance/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { RegisterForm } from './Components/RegisterForm';
 
 function App() {
   return (
     <div className="App">
-      
+      < RegisterForm />
     </div>
   );
 }
