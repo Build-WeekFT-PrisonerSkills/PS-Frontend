3e765c1e79fc0c8323ed620284ac577c69aac526
Sunil Karki <karkisunil1200@gmail.com>
2019-11-18 13:45:03 -0600

    Form created

--- second-chance/src/App.js
+++ second-chance/src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AddPrisonForm from './Components/AddPrison/AddPrisonForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      tps://reactjs.org" target="_b
+      <header className='App-header'>
+        <AddPrisonForm />
       </header>
     </div>
   );
--- second-chance/src/Components/AddPrison/AddPrisonForm.js
+++ second-chance/src/Components/AddPrison/AddPrisonForm.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const AddPrisonForm = () => {
+import {withFormik, Form, Field} from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+const AddPrisonForm = ({name, address, city, email, contactPhone, status, touched, errors}) => {
+  const [addPrison, setAddPrison] = useState([]);
   return (
     <div>
       <h1>Add Prison Form</h1>
+      <Form>
+        <label>Name</label>
+        <Field type='text' name='name' />
+
+        <label>Address</label>
+        <Field type='text' name='address' />
+
+        <label>City</label>
+        <Field type='City' name='city' />
+
+        <label>Email</label>
+        <Field type='text' name='email' />
+
+        <lable>Contact Phone</lable>
+        <Field type='text' name='contactPhone' />
+      </Form>
     </div>
   );
 };
-export default AddPrisonForm;
+
+const FormikAddPrisonForm = withFormik({
+  mapPropsToValues({name, address, city, email, contactPhone}) {
+    return {
+      name: name || '',
+      address: address || '',
+      city: city || '',
+      email: email || '',
+      contactPhone: contactPhone || ''
+    };
+  },
+
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required(),
+    address: Yup.string().required(),
+    city: Yup.string().required(),
+    email: Yup.string().required(),
+    contactPhone: Yup.string().required()
+  }),
+  handleSubmilt() {}
+})(AddPrisonForm);
+
+export default FormikAddPrisonForm;
