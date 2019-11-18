4b4cc2a2b3e05b91f7cf15d5effa35d0ae12de5b
Walter Futch <walter.futch@gmail.com>
2019-11-18 16:25:42 -0500

    Login is complete
    Login form is complete, minus styling

---
+++ second-chance/src/Components/Login.js
@@ -0,0 +1,97 @@
+import React, {useEffect} from 'react';
+import {Form, Field, withFormik} from 'formik';
+import axios from 'axios';
+import * as Yup from 'yup';
+import styled from 'styled-components';
+import '../App.css';
+
+const StyledErrorMessage = styled.p`
+  color: red;
+  margin: 10px;
+`
+
+const MainContainer = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  background: #fff;
+`
+
+const FormEntry = styled.div`
+  margin: 20px;
+`
+const ImgContainer = styled.div`
+  margin: 0 auto;
+`
+const FormContainer = styled.div`
+  margin: 0 auto;
+`
+
+const Title = styled.h1`
+  font-family: Lato;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 48px;
+  line-height: 58px;
+  /* identical to box height */
+  text-align: center;
+  color: #000000;
+`
+
+const LoginForm = ({ values, status, errors, touched, setUserToken, isSubmitting}) => {
+  useEffect(() => {
+    if (status) {
+      setUserToken(status)
+    }
+  }, [status])
+
+  return (
+    <MainContainer>
+
+    <ImgContainer>
+      <img src='https://images.unsplash.com/photo-1555963966-b7ae5404b6ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9' alt='electricion working on lines' />
+    </ImgContainer>
+    <FormContainer>
+      <Form>
+          <Title>Login</Title>
+          <FormEntry>
+            <Field className='loginEntry' type='email' name='username' placeholder='Email' />
+            {touched.username && errors.username && (<StyledErrorMessage>{errors.username}</StyledErrorMessage>)}
+          </FormEntry>
+          <FormEntry>
+            <Field className='loginEntry' type='password' name='password' placeholder='Password' />
+            {touched.password && errors.password && (<StyledErrorMessage>{errors.password}</StyledErrorMessage>)}
+          </FormEntry>
+          <button type='submit'>Login</button>
+      </Form>
+    </FormContainer>
+    </MainContainer>
+  )
+};
+
+const FormikLoginForm = withFormik({
+  mapPropsToValues({username, password}) {
+    return {
+      username: username || '',
+      password: password || ''
+    };
+  },
+
+  validationSchema: Yup.object().shape({
+    username: Yup.string().required('Must Enter a Username'),
+    password: Yup.string().required('Must Enter a Password')
+  }),
+
+  handleSubmit(values, {setStatus}) {
+    axios
+      .post('', values)
+      .then(res =>{
+        console.log(res);
+        setStatus(res.data);
+      })
+      .catch(err => console.log(err.res));
+  }
+})(LoginForm);
+
+export default FormikLoginForm
