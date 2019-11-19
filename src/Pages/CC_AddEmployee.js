b91bbda60a58554d4df7cfc81176c25ad164db89
Walter Futch <walter.futch@gmail.com>
2019-11-19 16:47:42 -0500

    Update CC_AddEmployee.js
    Removed Styled Components for now.

--- src/Pages/CC_AddEmployee.js
+++ src/Pages/CC_AddEmployee.js
@@ -22,29 +22,6 @@ const MyTextInput = ({ label, ...props }) => {
   );
 };
 
-// Styled components ....
-const StyledSelect = styled.select`
-  color: var(--blue);
-`;
-
-const StyledErrorMessage = styled.div`
-  font-size: 12px;
-  color: var(--red-600);
-  width: 400px;
-  margin-top: 0.25rem;
-  &:before {
-    content: "❌ ";
-    font-size: 10px;
-  }
-  @media (prefers-color-scheme: dark) {
-    color: var(--red-300);
-  }
-`;
-
-const StyledLabel = styled.label`
-  margin-top: 1rem;
-`;
-
 export const CC_AddEmployee = () => {
   return (
     <>
