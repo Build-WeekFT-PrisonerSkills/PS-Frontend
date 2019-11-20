7df57af67e2ad39d08ac711e6f8377d067821886
cjogles <jackson.ogles28@gmail.com>
2019-11-20 14:59:22 -0700

    nav bar links to home

--- src/Components/NavBarOther.js
+++ src/Components/NavBarOther.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom'
 export const NavBarOther = () => {
     return(
         <div style={{
@@ -9,7 +9,9 @@ export const NavBarOther = () => {
             display: 'flex', 
             alignItems: 'center',
             justifyContent: 'space-between'}}>
-            <h4>Second Chance</h4>
+            <Link to="/">
+                <h4>Second Chance</h4>
+            </Link>
             <div style={{display: "flex", justifyContent: 'space-evenly',}}>
     
             </div>
